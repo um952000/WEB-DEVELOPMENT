01c44d9bcf2ce12b387c29eed839fe7fd3252d09
Utkarsh Mishra <um952000@gmail.com>
2023-12-14 10:31:27 +0530

    mycommit

---
+++ Video55/script.js
@@ -0,0 +1,34 @@
+console.log("Hello I am Utkarsh");
+
+var a=5;
+var b=6;
+var c="Utkarsh"
+
+console.log(a+5+c+b);
+
+const z=10;
+
+//const variable cannot be changed
+
+// let has block scope
+
+// var has global scope
+
+// typeof(undefined) = undefined;
+// typeof(null) = object
+
+let obj = {
+
+    "name" : "Utkarsh",
+    "Role" : "Software Engineer"
+}
+
+console.log(obj);
+
+   // Adding new key value pair in the obj
+  obj.salary= "100cr";
+  console.log(obj);
+  
+//   changing value of the object in terms of its corresponding key
+  obj.salary= "500cr";
+  console.log(obj);
